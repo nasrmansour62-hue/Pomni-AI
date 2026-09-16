9ab55f7c5c6606a6ac0887a4cfcc947536a19091
nasrmansour62-hue <nasrmansour62@gmail.com>
2026-09-16 14:33:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,17 @@ import sub from './sub.js';
 const client = new Client({
   phoneNumber: '380719142698', // Bot number
   prefix: [".", "/", "!"],
-  fromMe: false,
+  fromMe: false, 
   owners: [
-    // Owner 1 - انت
+  // Owner 1
     { name: "الدشاش", lid: "6360947232863@lid", jid: "201211347034@s.whatsapp.net" },
-    // Owner 2
+  // Owner 2
     { name: "alhwary", lid: "201556853817@lid", jid: "201556853817@s.whatsapp.net" },
+  // Owner 3
+    { name: "عزازيل", jid: "201211347034@s.whatsapp.net", lid: "201211347034@lid" }
   ],
   settings: { noWelcome: false },
   commandsPath: './plugins'
-});
-
-// ===== كود اجباري عشان meowsab يتعرف على المطور =====
-client.ev.on('messages.upsert', async ({ messages }) => {
-    const m = messages[0];
-    if (!m) return;
-    const sender = m.key.participant || m.key.remoteJid;
-    const isOwner = client.config.owners.some(o => o.jid === sender || o.lid === sender);
-    m.isOwner = isOwner; // هنا بنقوله ده مطور
 });
 
 client.onGroupEvent(group);
@@ -37,17 +30,17 @@ if (!global.db) {
 
 /* =========== Config ========== */
 const { config } = client;
-config.info = {
-  nameBot: "𓆩𓃵𓆩𓆣𝓚𝓷𝓲𝓰𝓱𝓽 𝓐𝓵𝓓𝓪𝓼𝓱𝓪𝓼𝓱𓆣𓆪𓂀𓆪",
-  nameChannel: "𓆩𓃵𓆩𓆣𝓚𝓷𝓲𝓰𝓱𝓽 𝓐𝓵𝓓𝓪𝓼𝓱𝓪𝓼𝓱𓆣𓆪𓂀𓆪",
+config.info = { 
+  nameBot: "𓆩𓃵𓆩𓆣𝓚𝓷𝓲𝓰𝓱𝓽 𝓐𝓵𝓓𝓪𝓼𝓱𝓪𝓼𝓱𓆣𓆪𓂀𓆪", 
+  nameChannel: "𓆩𓃵𓆩𓆣𝓚𝓷𝓲𝓰𝓱𝓽 𝓐𝓵𝓓𝓪𝓼𝓱𝓪𝓼𝓱𓆣𓆪𓂀𓆪", 
   idChannel: "120363419296439517@newsletter",
   urls: {
     repo: "هنا رابط الريبو",
     api: "https://emam-api.web.id",
     channel: "https://whatsapp.com/channel/0029Vb6VF4R3bbUwgCtJlC3U"
   },
-  copyright: {
-    pack: '𓆩𓃵𓆩𓆣𝓚𝓷𝓲𝓰𝓱𝓽 𝓐𝓵𝓓𝓪𝓼𝓱𝓪𝓼𝓱𓆣𓆪𓂀𓆪',
+  copyright: { 
+    pack: '𓆩𓃵𓆩𓆣𝓚𝓷𝓲𝓰𝓱𝓽 𝓐𝓵𝓓𝓪𝓼𝓱𝓪𝓼𝓱𓆣𓆪𓂀𓆪', 
     author: '𓆩𓃵𓆩𓆣𝓚𝓷𝓲𝓰𝓱𝓽 𝓐𝓵𝓓𝓪𝓼𝓱𝓪𝓼𝓱𓆣𓆪𓂀𓆪'
   },
   images: [
@@ -61,10 +54,11 @@ config.info = {
 client.start();
 
 setTimeout(async () => {
-if (client.commandSystem) {
+if (client.commandSystem) { 
 sub(client)
   }
 }, 2000);
+
 
 /* =========== Catch Errors ========== */
 process.on('uncaughtException', (e) => {
@@ -74,3 +68,17 @@ process.on('uncaughtException', (e) => {
 process.on('unhandledRejection', (err) => {
     console.error('Unhandled Rejection:', err)
 });
+
+
+/* 
+=========== Memory Monitor ========== 
+
+setInterval(() => {
+    const used = process.memoryUsage().rss / 1024 / 1024
+    if (used > 800) {
+        console.log(`🔄 Bot memory full (${used.toFixed(1)}MB), restarting...`)
+        process.exit(1) 
+    }
+}, 300_000) 
+
+*/
